272f035edae3456fba923afbb209049579e7138f
邵家驹 <987585547@qq.com>
2024-04-02 23:19:46 +0800

    login detail

--- IM-Wc/IM-Wc/ViewModels/LoginViewModel.cs
+++ IM-Wc/IM-Wc/ViewModels/LoginViewModel.cs
@@ -9,28 +9,61 @@ using System.Windows;
 
 namespace IM_Wc.ViewModels
 {
-    public partial class LoginViewModel:ObservableObject
+    public partial class LoginViewModel : ObservableObject
     {
         Window window;
-        public string Name { get; set; } = "Test";
+
+        /// <summary>
+        /// 登录名
+        /// </summary>
         [ObservableProperty]
-        Visibility tdCodeVisibility=Visibility.Collapsed;
+        public string name = "Test";
+
+        /// <summary>
+        /// 扫码登录界面可见性
+        /// </summary>
         [ObservableProperty]
-        string tdCode="/Assets/tdcode.png";
+        Visibility tdCodeVisibility = Visibility.Collapsed;
+
+        /// <summary>
+        /// 是否正在请求二维码图片
+        /// </summary>
+        [ObservableProperty]
+        double imageOpacity=0.5;
+
+        [ObservableProperty]
+        bool isProcessBarActive;
+        /// <summary>
+        /// 二维码图片url，默认先读取本地资源文件，可更改为网络url
+        /// </summary>
+        [ObservableProperty]
+        string tdCode = "/Assets/tdcode.png";
+
+        /// <summary>
+        /// 登录窗口加载完后将窗体对象传给this.window字段，用于控制窗体返回结果和关闭
+        /// </summary>
+        /// <param name="win"></param>
         [RelayCommand]
         void Loaded(Window win)
         {
             window = win;
         }
+
+        /// <summary>
+        /// 切换账号按钮命令：使扫码登录和二维码可见，并请求后端发送二维码图片
+        /// </summary>
         [RelayCommand]
-        void SwitchAccount()
+        async Task SwitchAccount()
         {
+            IsProcessBarActive = true;
             TdCodeVisibility = Visibility.Visible;
+            TdCode = await PendingTDCode();
+            ImageOpacity = 1;
+            IsProcessBarActive = false;
         }
         [RelayCommand]
         async Task Login()
         {
-
             var r = await PendingLogin();
             if (r)
             {
@@ -42,13 +75,21 @@ namespace IM_Wc.ViewModels
                 MessageBox.Show("登录失败！");
             }
         }
-
+        async Task<string> PendingTDCode()
+        {
+            /*
+        *pending code Request
+        */
+            await Task.Delay(1000);
+            return "/Assets/tdcode.png";
+        }
         async Task<bool> PendingLogin()
         {
             /*
            *loginRequest
            */
-            return  true;
+            await Task.Delay(1000);
+            return true;
         }
     }
 }
