d4318eaa7c4829cef04da3e098ec5de4afcafad4
邵家驹 <987585547@qq.com>
2024-11-11 22:14:59 +0800

    fav list structure not done yet

---
+++ IM-Wc/IM-Wc/Models/FavoritesItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM_Wc.Models
+{
+    public enum FavoritesType
+    {
+        File = 0x01,
+        Image = 0x10,
+        All = File | Image
+    }
+    public class FavoritesItem
+    {
+        public string Icon { get; set; }
+        public string Name { get; set; }
+
+        public FavoritesType Type { get; set; }
+    }
+}
